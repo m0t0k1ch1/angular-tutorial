d4d2113844cfb998355724721ba6593c35944880
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-04-19 23:07:29 +0900

    fix: remove mock dir

--- src/app/mock/index.ts
+++
@@ -1,1 +0,0 @@
-export { HEROES } from './heroes';
--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -4,7 +4,17 @@ import { Observable, of } from 'rxjs';
 import { Hero } from 'src/app/model';
 import { MessageService } from 'src/app/service/message.service';
 
-import { HEROES } from 'src/app/mock';
+const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+];
 
 @Injectable({
   providedIn: 'root',
