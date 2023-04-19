438bf81d6b1b46086b0b7d75c96e9d4bfd68b425
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-04-19 23:02:05 +0900

    feat: use tailwindcss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,7 +23,13 @@ import { MessagesComponent } from './component/element/messages/messages.compone
 
     MessagesComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
