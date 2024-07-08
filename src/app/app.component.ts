01e6e2cfd7a59bf8e1a64286491fc97ebaa63335
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-07-09 01:22:45 +0900

    feat: add home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Hello world!</h1>`,
+  imports: [HomeComponent],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
