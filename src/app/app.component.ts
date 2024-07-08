d45fb4fcc594d170ba138ec1c7667fd8afc99624
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-07-09 03:11:41 +0900

    feat: set up routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [RouterModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'homes';
-}
+export class AppComponent {}
