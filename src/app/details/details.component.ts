731b97dadad499dd838f10c212813b6fab782130
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-07-09 03:22:24 +0900

    feat: implement details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { HousingLocation } from '../housing-location';
+
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-details',
@@ -7,4 +12,16 @@ import { Component } from '@angular/core';
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
-export class DetailsComponent {}
+export class DetailsComponent {
+  private route: ActivatedRoute = inject(ActivatedRoute);
+
+  private housingService: HousingService = inject(HousingService);
+
+  public housingLocation: HousingLocation | undefined;
+
+  constructor() {
+    const housingLocationID = Number(this.route.snapshot.params['id']);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationID);
+  }
+}
