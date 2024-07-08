bb6ea3b1c00ddc3b0b7a1e9bf321debdecacb351
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-07-09 00:52:27 +0900

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # angular-tutorial
+
+## Prerequisites
+
+Requires [pnpm](https://pnpm.io) to be installed.
+
+```
+pnpm install
+```
+
+## Run
+
+```
+pnpm start
+```
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  template: `<h1>Hello world!</h1>`,
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'homes';
+}
